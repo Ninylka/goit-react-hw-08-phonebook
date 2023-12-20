109553b7275b947eb01e3fb815d47e78aaf17bd1
Ninylka <shemetylka@gmail.com>
2023-12-20 14:30:17 +0100

    log

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,14 +1,11 @@
 
  
-import { Helmet } from 'react-helmet';
+
 import { RegisterForm } from 'components/RegisterForm/RegisterForm';
 
 export default function RegisterPage() {
   return (
     <div>
-      <Helmet>
-        <title>Registration</title>
-      </Helmet>
       <RegisterForm />
     </div>
   );
